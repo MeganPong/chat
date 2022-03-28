cab4a0418fbc1ab81b817fb7314236fe40dcab6a
Megan <meganpong88@yahoo.com.tw>
2022-03-28 20:11:48 +0800

    add chat1.py

---
+++ chat1.py
@@ -0,0 +1,12 @@
+
+lines = []
+with open('r1.txt', 'r') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print(name)
+
